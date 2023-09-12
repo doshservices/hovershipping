26607ae881d489db6ace9444bca1544d0a3d81e8
prosper edafe <mamusedafe19@gmail.com>
2023-09-12 14:32:28 +0100

    contact from created

---
+++ assets/js/form.js
@@ -0,0 +1,33 @@
+// const submit = document.getElementById("submit")
+
+// submit.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     const name = document.getElementById("name")
+//     const organization = document.getElementById("organization")
+//     const telephone = document.getElementById("telephone")
+//     const fax = document.getElementById("fax")
+//     const email = document.getElementById("email")
+//     const quoteorBooking = document.getElementById("QuoteorBooking")
+//     const type = document.getElementById("type")
+//     const origin = document.getElementById("origin")
+//     const destination = document.getElementById("destination")
+//     const brief = document.getElementById("brief")
+//     const value = document.getElementById("value")
+//     const size = document.getElementById("size")
+//     const weight = document.getElementById("weight")
+//     const volume = document.getElementById("volume")
+//     const date = document.getElementById("date")
+//     const requirements = document.getElementById("requirements")
+//     Email.send({
+//         SecureToken: "dc041d60-0db4-4ece-820d-b258e3cf036e",
+//         To: 'prosperj561@gmail.com',
+//         From: email.value,
+//         Subject: "This is the subject",
+//         Body: "And this is the body"
+//     }).then(
+//         message => alert(message)
+//     )
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// })
